38d73e19a2caccd50d743a854fd0d1ac7283b43d
PiyushJT <piyushthummar7@gmail.com>
2025-04-29 23:20:47 +0530

    Luck Casino Game

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 <img src="https://github.com/PiyushJT/iOS-Notes/raw/refs/heads/main/ss.png" alt="Screen Shot" width="300"/>
 
-<!-- 
 
 <img src="https://github.com/PiyushJT/iOS-Notes/raw/refs/heads/main/rec.gif" alt="Screen Shot" width="300"/>
 
- -->
--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -5,31 +5,117 @@ import SwiftUI
 struct ContentView: View {
     
     
+    @State var credits: Int = 0;
+    
+    @State var images: [String] = ["apple", "cherry", "star"];
+    
+    
     var body: some View {
         
-        ZStack {
+        VStack {
+            
+            Text("The Luck Game!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
             
             
-            Image("toronto")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(10)
-                .padding(20)
-                .border(.blue, width: 2)
+            Spacer()
             
             
-            VStack {
+            Text("Credits: " + String(credits))
+                .font(.title)
+            
+            
+            Spacer()
+            
+            HStack {
                 
-                Text("CN Tower")
-                    .font(.largeTitle)
+                Image(images[0])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                                
+                Image(images[1])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
                 
-                Text("Toronto")
-                    .font(.headline)
+                Image(images[2])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
                 
             }
             .padding()
-            .foregroundColor(.white)
-            .background(.black.opacity(0.85))
+            
+            Spacer()
+            
+            
+            Button(action: {
+                
+                
+                for i in 0..<3 {
+                    
+                    let imgNo = Int.random(in: 1...3)
+                    
+                    switch imgNo {
+                    case 1 :
+                        images[i] = "apple"
+                    case 2 :
+                        images[i] = "cherry"
+                    default :
+                        images[i] = "star"
+                    }
+                    
+                }
+                
+                if(images[0] == images[1]) {
+                    
+                    if(images[0] == images[2]) {
+                        credits += 100
+                    }
+                    else {
+                        credits += 50
+                    }
+                    
+                }
+                
+                else if(images[1] == images[2]) {
+                    
+                    if(images[1] == images[0]) {
+                        credits += 100
+                    }
+                    else {
+                        credits += 50
+                    }
+                    
+                }
+                
+                else if(images[2] == images[0]) {
+                    
+                    if(images[2] == images[1]) {
+                        credits += 100
+                    }
+                    else {
+                        credits += 50
+                    }
+                    
+                }
+                
+                
+            }, label: {
+                
+                Text("Spin")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 10)
+                    .background(Color.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(30)
+                
+                
+            })
+            
+            Spacer()
+            
             
         }
         
